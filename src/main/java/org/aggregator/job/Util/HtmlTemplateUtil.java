2ac370c21984eb51b7009b6f05c6dd4f030a993e
konianzero <konianzero@gmail.com>
2021-02-24 22:06:32 +0300

    Add Thymeleaf

---
+++ src/main/java/org/aggregator/job/Util/HtmlTemplateUtil.java
@@ -0,0 +1,31 @@
+package org.aggregator.job.Util;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import java.io.StringWriter;
+import java.util.List;
+
+import org.aggregator.job.vo.Vacancy;
+
+public class HtmlTemplateUtil {
+    public static String processHMTLTemplate(String templateName, List<Vacancy> vacancies) {
+        TemplateEngine templateEngine = new TemplateEngine();
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+
+        resolver.setPrefix("/templates/");
+        resolver.setSuffix(".html");
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        templateEngine.setTemplateResolver(resolver);
+
+        Context context = new Context();
+        context.setVariable("vacancies", vacancies);
+
+        StringWriter stringWriter = new StringWriter();
+        templateEngine.process(templateName, context, stringWriter);
+        return stringWriter.toString();
+    }
+}
--- src/main/java/org/aggregator/job/view/HtmlView.java
+++ src/main/java/org/aggregator/job/view/HtmlView.java
@@ -1,20 +1,16 @@
 package org.aggregator.job.view;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
-import java.util.Optional;
 
 import org.aggregator.job.Controller;
 import org.aggregator.job.vo.Vacancy;
+import org.aggregator.job.Util.HtmlTemplateUtil;
 
 public class HtmlView implements View {
-    private final String filePath = "src/main/java/" + this.getClass().getPackage().getName().replace('.', '/') + "/vacancies.html";
+    private final String filePath = "src/main/resources/vacancies.html";
     private Controller controller;
 
     @Override
@@ -25,53 +21,27 @@ public class HtmlView implements View {
     @Override
     public void update(List<Vacancy> vacancies) {
         try {
-            updateFile(updateContent(vacancies));
+            updateFile(createHTML(vacancies));
         } catch (Exception e) { e.printStackTrace(); }
     }
 
     private void updateFile(String str) {
-        try (FileWriter writer = new FileWriter(filePath)) {
-            writer.write(str);
-        } catch (IOException ioe) { ioe.printStackTrace(); }
-    }
-
-    private String updateContent(List<Vacancy> vacancies) {
-        Document document = null;
         try {
-            document = getDocument();
-            // Получи элемент, у которого есть класс template
-            Element template = document.getElementsByClass("template").first();
-            // Сделай копию этого объекта
-            Element templateCopy = template.clone();
-            // удали из нее атрибут "style" и класс "template"
-            templateCopy.removeAttr("style");
-            templateCopy.removeClass("template");
-            // Удали все добавленные ранее вакансии. У них единственный класс "vacancy"
-            // Но тег tr, у которого class="vacancy template", не удаляй.
-            document.select("tr[class=vacancy]").remove().not("tr[class=vacancy template]");
-
-            for (Vacancy vacancy : vacancies) {
-                Element localClone = templateCopy.clone();// клонируем шаблон тега
-                localClone.getElementsByClass("siteName").first().text(vacancy.getSiteName());
-                localClone.getElementsByClass("city").first().text(vacancy.getCity());
-                localClone.getElementsByClass("companyName").first().text(vacancy.getCompanyName());
-                localClone.getElementsByClass("salary").first().text(vacancy.getSalary());
-
-                Element link = localClone.getElementsByTag("a").first();
-                link.text(vacancy.getTitle());
-                link.attr("href", vacancy.getUrl());
-
-                template.before(localClone.outerHtml());
-            }
-
+            createFileIfNotExists();
+            Files.writeString(Path.of(filePath), str);
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
-        return Optional.ofNullable(document).map(Element::html).orElseThrow();
     }
 
-    protected Document getDocument() throws IOException {
-        return Jsoup.parse(Paths.get(filePath).toFile(), "UTF-8");
+    private void createFileIfNotExists() throws IOException {
+        if (Files.notExists(Path.of(filePath))) {
+            Files.createFile(Path.of(filePath));
+        }
+    }
+
+    private String createHTML(List<Vacancy> vacancies) {
+        return HtmlTemplateUtil.processHMTLTemplate("vacanciesTemplate", vacancies);
     }
 
     public void userCitySelectEmulationMethod() {
