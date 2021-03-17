0641a695f7065ac910d02e42c077308303e6f24a
konianzero <konianzero@gmail.com>
2021-03-17 23:44:01 +0300

    Refactoring HHStrategy.getElements(String): - removed parallel stream processing because of low efficiency

--- src/main/java/org/aggregator/job/model/strategy/HHStrategy.java
+++ src/main/java/org/aggregator/job/model/strategy/HHStrategy.java
@@ -4,9 +4,6 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.util.*;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -18,62 +15,33 @@ public class HHStrategy implements Strategy {
 
     private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java+%s&page=%d";
     private static final String SITE_NAME = "Head Hunter";
-    private ForkJoinPool customThreadPool;
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
-        createForkJoinPool();
-        List<Vacancy> vacancies = forkJoinSubmit(getVacancies(getElements(searchString)));
-        shutDownForkJoinPool();
-        return vacancies;
+        return getVacancies(getElements(searchString));
     }
 
-    private void createForkJoinPool() {
-        int poolSize = Runtime.getRuntime().availableProcessors();
-        customThreadPool = new ForkJoinPool(poolSize);
-    }
-
-    private void shutDownForkJoinPool() {
-        customThreadPool.shutdown();
-        try {
-            if (!customThreadPool.awaitTermination(500, TimeUnit.MILLISECONDS)) {
-                customThreadPool.shutdownNow();
-            }
-        } catch (InterruptedException e) {
-            customThreadPool.shutdownNow();
-        }
-    }
-
-    private <T> List<T> forkJoinSubmit(Callable<List<T>> listCallable) {
-        return customThreadPool.submit(listCallable).invoke();
-    }
-
-    private Callable<List<Vacancy>> getVacancies(List<Element> elementList) {
-        return () -> elementList.parallelStream()
-                                .map(this::getVacancy)
-                                .collect(Collectors.toList());
+    private List<Vacancy> getVacancies(List<Element> elementList) {
+        return elementList.stream()
+                          .map(this::getVacancy)
+                          .collect(Collectors.toList());
     }
 
     private List<Element> getElements(String searchString) {
-        return forkJoinSubmit(() ->
-                    Stream.iterate(0, i -> i + 1)
-                          .parallel()
-                          .map(pageNumber -> getDocument(String.format(URL_FORMAT, searchString, ++pageNumber)))
-                          .takeWhile(Optional::isPresent)
-                          .map(Optional::get)
-                          .map(document -> forkJoinSubmit(getElements(document)))
-                          .map(elements -> elements.parallelStream())
-                          .reduce(Stream::concat).orElse(Stream.empty())
-                          .collect(Collectors.toList())
-        );
+        return Stream.iterate(0, i -> i + 1)
+                     .map(pageNumber -> getDocument(String.format(URL_FORMAT, searchString, ++pageNumber)))
+                     .takeWhile(Optional::isPresent)
+                     .map(Optional::get)
+                     .map(this::getElements)
+                     .flatMap(Collection::stream)
+                     .collect(Collectors.toList());
     }
 
-    private Callable<List<Element>> getElements(Document document) {
-        return () -> document
-                .select("div[data-qa=vacancy-serp__vacancy]")
-                .parallelStream()
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
+    private List<Element> getElements(Document document) {
+        return document.select("div[data-qa=vacancy-serp__vacancy]")
+                       .stream()
+                       .filter(Objects::nonNull)
+                       .collect(Collectors.toList());
     }
 
     private Vacancy getVacancy(Element element) {
