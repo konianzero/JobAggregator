696fab646d3716f7f903b213a98bca847498d240
konianzero <konianzero@gmail.com>
2022-02-08 20:23:48 +0300

    refactor: Refactor CareerHabrStrategy.java

--- src/main/java/org/aggregator/job/model/strategy/CareerHabrStrategy.java
+++ src/main/java/org/aggregator/job/model/strategy/CareerHabrStrategy.java
@@ -1,77 +1,88 @@
 package org.aggregator.job.model.strategy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.net.URI;
-import java.net.http.HttpRequest;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.aggregator.job.to.Vacancy;
 
-import static org.aggregator.job.util.Util.*;
+import static java.util.function.Predicate.not;
+import static org.aggregator.job.util.Util.getDocument;
 
+@Slf4j
 public class CareerHabrStrategy implements Strategy {
 
-    private static final String URL_FORMAT = "https://career.habr.com/vacancies?city_id=%s&page=%d&q=java&type=all";
+    private static final String URL = "https://career.habr.com/vacancies?city_id=%s&page=%d&q=java&type=all";
     private static final String SITE_NAME = "Хабр Карьера";
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
-        return getVacancies(getElements(searchString));
+        return vacanciesFrom(processPage(searchString));
     }
 
-    private List<Vacancy> getVacancies(List<Element> elementList) {
-        List<Vacancy> vacancies = new ArrayList<>();
-
-        for (Element element : elementList) {
-            if (element == null) { continue; }
-
-            vacancies.add(getVacancy(element));
-        }
-
-        return vacancies;
+    private List<Vacancy> vacanciesFrom(List<Element> vacanciesElements) {
+        return vacanciesElements.stream()
+                .map(this::mapToVacancy)
+                .collect(Collectors.toList());
     }
 
-    private List<Element> getElements(String searchString) {
+    private List<Element> processPage(String searchString) {
         List<Element> elements = new ArrayList<>();
-        int pageNumber = 0;
         String cityId = getCityId(searchString);
 
         if (Objects.isNull(cityId)) { return elements; }
 
-        while (true) {
-            Optional<Elements> vacancyElements = getDocument(String.format(URL_FORMAT, cityId, ++pageNumber))
-                    .map(doc -> doc.getElementsByClass("vacancy-card"));
-
-            if (vacancyElements.isEmpty() || vacancyElements.get().size() == 0) { break; }
-
-            elements.addAll(vacancyElements.get());
-        }
-
-        return elements;
+        return Stream.iterate(0, i -> i + 1)
+                .map(pageNumber -> getDocument(String.format(URL, cityId, pageNumber)))
+                .takeWhile(Optional::isPresent)
+                .map(Optional::get)
+                .map(this::getVacanciesElements)
+                .takeWhile(not(ArrayList::isEmpty))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
     }
 
-    private Vacancy getVacancy(Element element) {
+    private Elements getVacanciesElements(Document document) {
+        return document.getElementsByClass("vacancy-card");
+    }
+
+    private Vacancy mapToVacancy(Element element) {
         Vacancy vacancy = new Vacancy();
 
         vacancy.setTitle(element.getElementsByClass("vacancy-card__title").text());
         String salary = element.getElementsByClass("basic-salary").text();
         vacancy.setSalary(salary == null ? "" : salary);
-        vacancy.setCity(element.getElementsByClass("vacancy-card__meta").first().child(0).text());
+        vacancy.setCity(element.getElementsByClass("vacancy-card__meta").text().replace(" · ", ", "));
         vacancy.setCompanyName(element.getElementsByClass("vacancy-card__company-title").text());
         vacancy.setSiteName(SITE_NAME);
         vacancy.setUrl("https://career.habr.com" + element.getElementsByClass("vacancy-card__title").first().child(0).attr("href"));
 
         return vacancy;
     }
+
+
+    /* *************************************
+     * *** Helper methods to get city ID ***
+     * *************************************/
+
+    public static final JSONParser JSON_PARSER = new JSONParser();
+    private static final HttpClient httpClient = HttpClient.newHttpClient();
 
     private String getCityId(String searchString) {
         return parseJsonResponse(getResponseString(makeRequest(searchString)));
@@ -89,10 +100,23 @@ public class CareerHabrStrategy implements Strategy {
         return res;
     }
 
+    public static String getResponseString(HttpRequest request) {
+        HttpResponse<String> response = null;
+        try {
+            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            log.error("I/O Exception when send request to " + request.uri());
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+            Thread.currentThread().interrupt();
+        }
+
+        return Optional.ofNullable(response).map(HttpResponse::body).orElseThrow();
+    }
+
     private HttpRequest makeRequest(String searchString) {
-        URI locationUri = URI.create(String.format("https://career.habr.com/api/frontend/suggestions/locations?term=%s", searchString));
         return HttpRequest.newBuilder()
-                          .uri(locationUri)
+                          .uri(URI.create("https://career.habr.com/api/frontend/suggestions/locations?term=" + searchString))
                           .GET()
                           .build();
     }
--- src/main/java/org/aggregator/job/util/Util.java
+++ src/main/java/org/aggregator/job/util/Util.java
@@ -2,15 +2,12 @@ package org.aggregator.job.util;
 
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
-import org.json.simple.parser.JSONParser;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 import java.lang.reflect.Proxy;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.Optional;
 
 import org.aggregator.job.model.strategy.Strategy;
@@ -18,32 +15,16 @@ import org.aggregator.job.model.strategy.Strategy;
 @UtilityClass
 @Slf4j
 public class Util {
-    public static final JSONParser JSON_PARSER = new JSONParser();
-    private static final HttpClient httpClient = HttpClient.newHttpClient();
 
     public static Optional<Document> getDocument(String str) {
         Document doc;
         try {
             doc = Jsoup.connect(str).get();
         } catch (IOException ioe) {
-            log.error("I/O Exception when Jsoup get document from " + str);
+            log.warn("I/O Exception when Jsoup get document from " + str);
             return Optional.empty();
         }
         return Optional.ofNullable(doc);
-    }
-
-    public static String getResponseString(HttpRequest request) {
-        HttpResponse<String> response = null;
-        try {
-            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException e) {
-            log.error("I/O Exception when send request to " + request.uri());
-        } catch (InterruptedException e) {
-            log.error(e.getMessage());
-            Thread.currentThread().interrupt();
-        }
-
-        return Optional.ofNullable(response).map(HttpResponse::body).orElseThrow();
     }
 
     @SuppressWarnings("unchecked")
