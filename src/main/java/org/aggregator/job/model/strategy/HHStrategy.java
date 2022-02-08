f3a65b07d516108f16e14a90e3d4032d2b7b64ea
konianzero <konianzero@gmail.com>
2022-02-08 18:41:40 +0300

    refactor: Refactor HHStrategy.java

--- src/main/java/org/aggregator/job/model/strategy/HHStrategy.java
+++ src/main/java/org/aggregator/job/model/strategy/HHStrategy.java
@@ -13,46 +13,46 @@ import static org.aggregator.job.util.Util.getDocument;
 
 public class HHStrategy implements Strategy {
 
-    private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java+%s&page=%d";
+    private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java+developer+%s&page=%d";
     private static final String SITE_NAME = "Head Hunter";
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
-        return getVacancies(getElements(searchString));
+        return vacanciesFrom(processPage(searchString));
     }
 
-    private List<Vacancy> getVacancies(List<Element> elementList) {
-        return elementList.stream()
-                          .map(this::getVacancy)
+    private List<Vacancy> vacanciesFrom(List<Element> vacancyElements) {
+        return vacancyElements.stream()
+                          .map(this::mapToVacancy)
                           .collect(Collectors.toList());
     }
 
-    private List<Element> getElements(String searchString) {
+    private List<Element> processPage(String searchString) {
         return Stream.iterate(0, i -> i + 1)
-                     .map(pageNumber -> getDocument(String.format(URL_FORMAT, searchString, ++pageNumber)))
+                     .map(pageNumber -> getDocument(String.format(URL_FORMAT, searchString, pageNumber)))
                      .takeWhile(Optional::isPresent)
                      .map(Optional::get)
-                     .map(this::getElements)
+                     .map(this::getVacanciesElements)
                      .flatMap(Collection::stream)
                      .collect(Collectors.toList());
     }
 
-    private List<Element> getElements(Document document) {
-        return document.select("div[data-qa=vacancy-serp__vacancy]")
+    private List<Element> getVacanciesElements(Document document) {
+        return document.select("div[data-qa^='vacancy-serp__vacancy ']")
                        .stream()
                        .filter(Objects::nonNull)
                        .collect(Collectors.toList());
     }
 
-    private Vacancy getVacancy(Element element) {
+    private Vacancy mapToVacancy(Element element) {
         Vacancy vacancy = new Vacancy();
 
-        vacancy.setTitle(element.select("a[data-qa$=vacancy-title]").text());
-        vacancy.setSalary(Optional.ofNullable(element.select("span[data-qa$=vacancy-compensation]").text()).orElse(""));
-        vacancy.setCity(element.select("span[data-qa$=vacancy-address]").text());
-        vacancy.setCompanyName(element.select("a[data-qa$=vacancy-employer]").text());
+        vacancy.setTitle(element.select("a[data-qa=vacancy-serp__vacancy-title]").text());
+        vacancy.setSalary(Optional.ofNullable(element.select("span[data-qa=vacancy-serp__vacancy-compensation]").text()).orElse(""));
+        vacancy.setCity(element.select("div[data-qa=vacancy-serp__vacancy-address]").text());
+        vacancy.setCompanyName(element.select("a[data-qa=vacancy-serp__vacancy-employer]").text());
         vacancy.setSiteName(SITE_NAME);
-        vacancy.setUrl(element.select("a[data-qa$=vacancy-title]").attr("href"));
+        vacancy.setUrl(element.select("a[data-qa=vacancy-serp__vacancy-title]").attr("href"));
 
         return vacancy;
     }
